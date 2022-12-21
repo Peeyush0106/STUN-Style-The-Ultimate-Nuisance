a9a09effef00c05e931dec484ef89d66c147fa8b
Peeyush0106 <peeyush.agarwal609@gmailcom>
2022-12-21 19:15:01 +0530

    notes app

--- signed-in_secret-notes_sketch.js
+++ signed-in_secret-notes_sketch.js
@@ -14,6 +14,10 @@ function checkConnectionEverySecond() {
         var connectedRef = database.ref(".info/connected");
         connectedRef.on("value", function (snap) {
             if (snap.val()) {
+                if (document.getElementById("note-display").innerHTML === "\n\n        " && getUrlParams()["note"]) {
+                    document.getElementById("note-display").innerHTML = "";
+                    openNote(getUrlParams()["note"]);
+                }
                 animationStarted = false;
                 canvas.elt.hidden = true;
                 if (!firstTimeZoomedIn) {
@@ -66,7 +70,8 @@ function getNotes() {
                 document.getElementById("notes").appendChild(document.createElement("br"));
 
                 document.getElementById("noteP-" + note.key).addEventListener("click", () => {
-                    openNote(note.key);
+                    setURLParam("note", note.key);
+                    // openNote(note.key);
                 });
             }
         }
@@ -138,6 +143,7 @@ function secretNotesHome() {
         document.getElementById("body").style.transform = "scale(0.01)";
         setTimeout(function () {
             document.getElementById("add-note-elts").hidden = true;
+            document.getElementById("add-note").hidden = false;
             if (document.getElementById("note-does-not-exist")) {
                 document.getElementById("note-does-not-exist").parentElement.removeChild(document.getElementById("note-does-not-exist"));
             }
@@ -166,7 +172,9 @@ function openNote(key) {
         document.getElementById("body").style.transition = ".6s";
         document.getElementById("body").style.transform = "scale(0.01)";
         setTimeout(function () {
+            document.getElementById("add-note").hidden = true;
             document.getElementById("notes").innerHTML = "";
+            document.getElementById("note-display").innerHTML = "";
             document.getElementById("back-btn").hidden = false;
             database.ref("Users/" + auth.currentUser.uid + "/Secret Notes/" + key).get().then((data) => {
                 if (data.exists()) {
@@ -212,4 +220,36 @@ function backToHome() {
             location.href = "signed-in_index.html";
         }, 600);
     }, 100);
+}
+
+function setURLParam(name, key) {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set(name, key);
+    window.location.search = urlParams;
+}
+
+function getUrlParams() {
+    var vars = {};
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function removeParam(key, sourceURL) {
+    var rtn = sourceURL.split("?")[0],
+        param,
+        params_arr = [],
+        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    if (queryString !== "") {
+        params_arr = queryString.split("&");
+        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+            param = params_arr[i].split("=")[0];
+            if (param === key) {
+                params_arr.splice(i, 1);
+            }
+        }
+        if (params_arr.length) rtn = rtn + "?" + params_arr.join("&");
+    }
+    return rtn;
 }
